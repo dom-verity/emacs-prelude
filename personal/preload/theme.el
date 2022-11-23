e94f1a90d4e2a6712819ba3b2a26ba5151b24cf3
Dominic Verity <dominic.verity@mq.edu.au>
2022-11-23 12:50:20 +1100

    Add `solarized` theme to prelude-packages and enable it.

---
+++ personal/preload/theme.el
@@ -0,0 +1,13 @@
+;;; theme.el --- Dom's Prelude Config: Theme and font setup.
+;;
+;; Copyright © 2022 Dom Verity
+;;
+;; Author: Dom Verity <dom.verity@gmail.com>
+;; URL: https://github.com/dom-verity/prelude
+
+;; This file is not part of GNU Emacs.
+
+;; Commentary
+
+(setq prelude-theme 'solarized-light)
+(set-face-attribute 'default nil :font "Office Code Pro-14")
