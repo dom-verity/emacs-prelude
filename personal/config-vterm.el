5a82d526eac70899cf85e8124dd3cbf353fed694
Dominic Verity <dominic.verity@mq.edu.au>
2022-11-23 18:19:07 +1100

    Add keybindings and settings for vterm package.

---
+++ personal/config-vterm.el
@@ -0,0 +1,14 @@
+;;; config-vterm.el --- Dom's Prelude Config: vterm settings
+;;
+;; Copyright © 2022 Dom Verity
+;;
+;; Author: Dom Verity <dom.verity@gmail.com>
+;; URL: https://github.com/dom-verity/prelude
+
+;; This file is not part of GNU Emacs.
+
+;; Commentary
+
+;; Keybindings and settings for the vterm terminal.
+
+(global-set-key (kbd "C-x M-m") 'vterm-other-window)
