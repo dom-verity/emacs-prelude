fb65b79ebc7df12058570600a2225ec78fc20e62
Dominic Verity <dominic.verity@mq.edu.au>
2022-11-23 12:49:10 +1100

    When on MacOS use the GNU standard utilities in dired.

---
+++ personal/preload/gnutils.el
@@ -0,0 +1,20 @@
+;;; gnutils.el --- Dom's Prelude Config: Enable use of gnu utils by dired.
+;;
+;; Copyright © 2022 Dom Verity
+;;
+;; Author: Dom Verity <dom.verity@gmail.com>
+;; URL: https://github.com/dom-verity/prelude
+
+;; This file is not part of GNU Emacs.
+
+;; Commentary
+
+;; On MacOS configure dired to use GNU versions of the standard 
+;; utilities ls, df, chmod etc.
+
+;; Get these by installing the homebrew `coreutils` package.
+
+(setq-default insert-directory-program "gls")
+(setq-default directory-free-space-program "gdf")
+(setq-default dired-chmod-program "gchmod")
+(setq-default dired-chown-program "gchown")
