1111bcfa7a4d00a90a4f12756aca0a3c286b6507
Dominic Verity <dominic.verity@mq.edu.au>
2023-07-26 12:51:22 +1000

    configure tree-sitter languages alist

--- personal/common-init.el
+++ personal/common-init.el
@@ -43,4 +43,23 @@
 ;; Use ivy to do flyspell correction
 (define-key flyspell-mode-map (kbd "C-c $") 'flyspell-correct-wrapper)
 
+;; Setup tree-sitter available languages alist
+(setq treesit-language-source-alist
+   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (cmake "https://github.com/uyha/tree-sitter-cmake")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+     (fish "https://github.com/ram02z/tree-sitter-fish")
+     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (make "https://github.com/alemuller/tree-sitter-make")
+     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (toml "https://github.com/tree-sitter/tree-sitter-toml")
+     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
 ;;; common-init.el ends here
